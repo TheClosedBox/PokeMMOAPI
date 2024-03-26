17941403a0cf06557cb241fa0fc746bef38aadf4
Emilio Gomez <emiliogmztj@gmail.com>
2024-03-25 19:24:13 -0700

    Added endpoints for moves and items.

--- app.py
+++ app.py
@@ -10,11 +10,27 @@ load_dotenv()
 redis_host = os.getenv("REDIS_HOST")
 redis_port = os.getenv("REDIS_PORT")
 redis_password = os.getenv("REDIS_PASSWORD")
-redis_db = os.getenv("REDIS_DB")
-redis_client = redis.Redis(host=redis_host, port=redis_port, password=redis_password, db=redis_db)
+pokemon_db = os.getenv("POKEMON_DB")
+item_db = os.getenv("ITEM_DB")
+move_db = os.getenv("MOVE_DB")
+pokemon_client = redis.Redis(host=redis_host, port=redis_port, password=redis_password, db=pokemon_db)
+item_client = redis.Redis(host=redis_host, port=redis_port, password=redis_password, db=item_db)
+move_client = redis.Redis(host=redis_host, port=redis_port, password=redis_password, db=move_db)
 
 def get_pokemon(dex):
-    return redis_client.get(dex)
+    print('Get Pokemon #' + str(dex))
+    print(pokemon_client)
+    return pokemon_client.get(dex)
+
+def get_item(id):
+    print('Get Item #' + str(id))
+    print(item_client)
+    return item_client.get(id)
+
+def get_move(id):
+    print('Get Move #' + str(id))
+    print(move_client)
+    return move_client.get(id)
 
 @app.route('/')
 def hello():
@@ -27,3 +43,11 @@ def celina():
 @app.route('/api/pokemmo/pokemon/<int:dex>')
 def pokemon(dex):
     return get_pokemon(dex)
+
+@app.route('/api/pokemmo/item/<int:id>')
+def item(id):
+    return get_item(id)
+
+@app.route('/api/pokemmo/move/<int:id>')
+def move(id):
+    return get_move(id)
